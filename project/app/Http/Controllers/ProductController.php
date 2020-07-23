57f244c5e0dfd07955641b6a5daa22ee1e38492e
Renisson Silva <renissonx@gmail.com>
2020-07-23 13:22:38 -0300

    Limpando código

--- project/app/Http/Controllers/ProductController.php
+++ project/app/Http/Controllers/ProductController.php
@@ -11,11 +11,7 @@ class ProductController extends Controller
     public function __construct(){
         $this->middleware('auth')->except(['index','show']);
     }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         $products = Product::get();
@@ -23,22 +19,11 @@ class ProductController extends Controller
 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('products.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $request->validate([
@@ -65,38 +50,18 @@ class ProductController extends Controller
             ->with('success','Product created successfully.');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //fazer verificação se existe
         $product = Product::findOrFail($id);
         return view('products.show',compact('product'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         $product = Product::findOrFail($id);
         return view('products.edit',compact('product'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $image_name = $request->hidden_image;
@@ -130,16 +95,9 @@ class ProductController extends Controller
             ->with('success','Product updated successfully.');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Product $product)
     {
         $product->delete();
-
         return redirect()->route('products.index')
             ->with('success','Product deleted successfully.');
     }
