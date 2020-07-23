2052e7d03310020aae61d55cf3d1788f35c00470
Renisson Silva <renissonx@gmail.com>
2020-07-22 21:08:49 -0300

    Estilizando página do create

--- project/resources/views/products/create.blade.php
+++ project/resources/views/products/create.blade.php
@@ -1,16 +1,9 @@
 @extends('products.layout')
 
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Create new product</h2>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
-        </div>
-    </div>
-</div>
+
+    <span class="titleProducts">Create new product</span>
+
 
 @if ($errors->any())
     <div class="alert alert-danger">
@@ -28,27 +21,28 @@
      <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Name:</strong>
+                <strong class="nameDesc">Name:</strong>
                 <input type="text" name="name" class="form-control" placeholder="name">
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Description:</strong>
+                <strong class="nameDesc">Description:</strong>
                 <textarea class="form-control" style="height:280px" name="description" placeholder="Description"></textarea>
             </div>
         </div>
 
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Select product image:</strong>
+                <strong class="nameDesc">Select product image:</strong>
                 <input type="file" name="image">
             </div>
         </div>
 
 
         <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Submit</button>
+            <a class="btn btn-outline-info btn-lg" href="{{ route('products.index') }}"> Back</a>
+            <button type="submit" class="btn btn-info btn-lg">Submit</button>
         </div>
     </div>
 
